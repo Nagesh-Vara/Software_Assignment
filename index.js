1c5d10e9478bdf3d7678a9bec38c1ec3d08a0c22
Nagesh-Vara <115449731+Nagesh-Vara@users.noreply.github.com>
2022-10-11 14:54:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ function displayResult(avgTotal) {
   document.getElementById('pcost').innerHTML = costOfProject;
   document.getElementById('effort').innerHTML = effortOfProject;
 }
+function myCreateFunction() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(-1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(-1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+  cell3.innerHTML = "NEW CELL 3";
+}
